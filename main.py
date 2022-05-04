3324c50d033b9c8805a5535bc5a760b8966ae3f8
Bukola Testimony <>
2022-05-04 20:09:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+# To make our own functions:
+def my_function():
+  print("Hello")
+  print("Love")
+my_function()
+
+def mum():
+  name = input("what is your name?: ")
+  place = 3
+  print(f"{name} let us meet at my {place}")
+mum()
+
+# To all the function that i have defined ie to be able to execute my function use:
+my_function() 
+# this should have same indentation with (def)
